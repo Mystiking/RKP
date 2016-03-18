4aa5c7911df227afa272b9f35ec741c57163f248
mystiking <maxkragballe@gmail.com>
2016-03-18 14:57:01 +0100

    Added admin.py for admin functions

--- RKP/app.py
+++ RKP/app.py
@@ -22,23 +22,6 @@ def list_members():
     else:
         return render_template('startPage.html', members=members, unknown=1)
 
-@app.route('/load_members', methods=['GET', 'POST'])
-def load_m():
-    names = db.session.query(Member).order_by(Member.pos).all()
-    with open('members.txt') as fp:
-        for line in fp:
-            flag = 0
-            new_member = Member(line, 0)
-            for n in names:
-                if n.lower() == line.lower():
-                    flag = 1
-            if flag == 0:
-                db.session.add(new_member)
-    db.session.commit()
-    db.session.flush()
-    members = db.session.query(Member).order_by(Member.name).all()
-    return render_template('admin.html', members=members)
-
 @app.route('/rkp', methods=['GET'])
 def follow_the_rules():
     return render_template('rules.html')
@@ -54,91 +37,6 @@ def log_in():
         return render_template('admin.html', members=members)
 
 
-@app.route('/give_list_rkp', methods=['GET', 'POST'])
-def give_list():
-    names = (request.form['names'])
-    msg = request.form['reason']
-    rkp = int(request.form['rkp'])
-    members = db.session.query(Member).all()
-    names = names.split(", ")
-    for n in names:
-        db.session.query(Member).filter(Member.name == n).first().rkp += rkp
-        db.session.query(Member).filter(Member.name == n).first().latest = msg
-        db.session.query(Member).filter(Member.name == n).first().change = rkp
-        message = Message(n, msg, db.session.query(Member).filter(Member.name == n).index)
-        message.rkp = rkp
-        db.session.add(message)
-    db.session.commit()
-    db.session.flush()
-    dir = 'logs'
-
-    if not os.path.exists(dir):
-        os.makedirs(dir)
-
-    path = str(datetime.datetime.now())[0:19] + '.txt'
-    log = open(os.path.join(dir, path), 'w')
-    members = db.session.query(Member).order_by(Member.name).all()
-    for m in members:
-        log.write(m.name + ' :\n')
-        messages = db.session.query(Message).order_by(Message.index).all()
-        for msg in messages:
-            if msg.key == m.index:
-                log.write("Message: " + msg.msg + " RKP : " + str(msg.rkp) + '\n')
-    log.close()
-    return render_template('admin.html', members=members)
-
-
-
-@app.route('/give_rkp', methods=['GET', 'POST'])
-def give():
-    id = int(request.form['hidden'])
-    reason = request.form['reason']
-    rkp = int(request.form['amount'])
-    members = db.session.query(Member).order_by(Member.name).all()
-    db.session.query(Member).filter(Member.index == id).first().rkp += rkp
-    db.session.query(Member).filter(Member.index == id).first().latest = reason
-    db.session.query(Member).filter(Member.index == id).first().change = rkp
-    message = Message(db.session.query(Member).filter(Member.index == id).first().name, reason, id)
-    message.rkp = rkp
-    db.session.add(message)
-    db.session.commit()
-    db.session.flush()
-
-    dir = 'logs'
-
-    if not os.path.exists(dir):
-        os.makedirs(dir)
-
-    path = str(datetime.datetime.now())[0:19] + '.txt'
-
-    log = open(os.path.join(dir, path), 'w')
-    for m in members:
-        log.write(m.name + ' :\n')
-        messages = db.session.query(Message).order_by(Message.index).all()
-        for msg in messages:
-            if msg.key == m.index:
-                 log.write("Message: " + msg.msg + " RKP : " + str(msg.rkp) + '\n')
-    log.close()
-    return render_template('admin.html', members=members)
-
-
-@app.route('/add_member', methods=['POST'])
-def add_member():
-    name = request.form['member']
-    rkp = int(request.form['rkp'])
-    new_member = Member(name, rkp)
-    members = db.session.query(Member).order_by(Member.pos).all()
-    for m in members:
-        if m.name == name:
-            return render_template('admin.html', members=members)
-    db.session.add(new_member)
-    db.session.flush()
-    db.session.commit()
-    members = db.session.query(Member).order_by(Member.pos).all()
-    with open('members.txt', 'a') as f:
-        f.write(name + '\n')
-    return render_template('admin.html', members=members)
-
 
 @app.route('/login_user', methods=['GET', 'POST'])
 def log_in_user():
@@ -152,18 +50,6 @@ def log_in_user():
     return render_template('login.html',
                            backname='Back to signup',
                            backlink='signup.html')
-
-
-@app.route('/signup', methods=['GET', 'POST'])
-def sign_up():
-    if request.method == 'POST':
-        name = request.form['username']
-        password = request.form['password']
-        if not db.session.query(User).filter(User.name == name).first():
-            db.session.add(User(name, password))
-            db.session.commit()
-            return redirect('/login_user')
-    return render_template('signup.html')
 
 
 @app.route('/logout', methods=['GET', 'POST'])
@@ -189,15 +75,6 @@ def go_to():
     messages = db.session.query(Message).filter(Message.key == key).order_by(Message.index).all()
     return render_template('/members/member.html', messages=messages, name=name, picture=picture)
 
-
-
-@app.route('/delete_member', methods=['POST', 'GET'])
-def delete_member():
-    index = int(request.form['del'])
-    db.session.query(Member).filter(Member.index == index).delete()
-    db.session.commit()
-    members = db.session.query(Member).order_by(Member.pos).all()
-    return render_template('admin.html', members=members)
 
 
 if __name__ == '__main__':
